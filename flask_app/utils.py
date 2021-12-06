246e246ac96196c7afec126cacaae92580138e1d
Jahanzeb Hassan <jahanzebhassan1234@gmail.com>
2021-12-05 20:49:35 -0500

    date fix

--- flask_app/utils.py
+++ flask_app/utils.py
@@ -1,5 +1,11 @@
 from datetime import timedelta
+import os
 
 def convert_datetime(datetime):
-	local = datetime - timedelta(hours=5)
-	return local.strftime("%B %d, %Y %I:%M %p")
+	is_prod = os.environ.get('IS_HEROKU', None)
+
+	if is_prod:
+		# Atlas uses UTC, so here I'm just converting to EST.
+		datetime = datetime - timedelta(hours=5)
+
+	return datetime.strftime("%B %d, %Y %I:%M %p")
